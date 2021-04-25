5fa03a96319b333eb948eca49a642c5ce4043bf7
Noah Day <sheltielover432@gmail.com>
2021-04-25 17:12:41 -0400

    API Setup

---
+++ LandmarkAI/Classes/Tags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandmarkAI.Classes
+{
+    public class Predicition
+    {
+        public string TagID { get; set; }
+        public string Tag { get; set; }
+        public double Probability { get; set; }
+    }
+
+    public class CustomVision
+    {
+        public string ID { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public IList<Predicition> Predictions { get; set; }
+    }
+}
--- LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,9 +23,21 @@ namespace LandmarkAI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string urlKey, predictionKey, contentKey;
+
         public MainWindow()
         {
             InitializeComponent();
+            ParseKeyFile();
+        }
+
+        private void ParseKeyFile()
+        {
+            //Change file directory to mattch keys
+            string[] lines = File.ReadAllLines(@"F:\Github\WPF_VisualIntelligence\LandmarkAI\keys.txt");
+            urlKey = lines[0];
+            predictionKey = lines[1];
+            contentKey = lines[2];
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -35,6 +49,22 @@ namespace LandmarkAI
             {
                 string fileName = dialog.FileName;
                 selectedImage.Source = new BitmapImage(new Uri(fileName));
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            var file = File.ReadAllBytes(fileName);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentKey);
+                    var response = await client.PostAsync(urlKey, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                }
             }
         }
     }
